6c57600d0f39ea34c61e061f225c187effe42d60
Ghitado <thiaguinho.020405@gmail.com>
2024-11-07 12:33:06 -0300

    feat: adding response error json

--- src/Shared/MyRecipeBook.Communication/Responses/ResponseErrorJson.cs
+++ src/Shared/MyRecipeBook.Communication/Responses/ResponseErrorJson.cs
@@ -3,13 +3,15 @@
     public class ResponseErrorJson
     {
         public IList<string> Errors { get; set; }
+        
         public ResponseErrorJson(IList<string> errors) => Errors = errors;
+
         public ResponseErrorJson(string error)
         {
             Errors = new List<string>
-        {
-            error
-        };
+            {
+                error
+            };
         }
     }
 }
